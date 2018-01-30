7a53919b880695aff6f5f1ed0c2ea4f1f7633ee8
ANYCUBIC-3D <34119888+ANYCUBIC-3D@users.noreply.github.com>
2018-01-30 10:08:29 +0800

    Add files via upload

---
+++ Marlin_TFT_CR_V1.1.1/configuration_store.h
@@ -0,0 +1,80 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef CONFIGURATION_STORE_H
+#define CONFIGURATION_STORE_H
+
+#include "MarlinConfig.h"
+
+void Config_ResetDefault();
+void Config_StoreSettings();
+
+
+#ifdef OutageTest
+
+static bool RestartFlag=false;
+void OutageSave();
+void OutageRead();
+extern float last_position[4];
+extern long last_sd_position[1];
+#endif
+void SaveMyZoffset();
+void ReadMyZoffset();
+extern float Current_z_offset;
+//extern float last_z_offset[1];
+extern float last_z_offset;
+extern unsigned char FirstBootFlag;
+void SaveFirstBootFlag();
+void readFirstBootFlag();
+
+extern float Current_z_offset;
+//extern float last_z_offset[1];
+
+
+extern unsigned char FirstBootFlag;
+void SaveFirstBootFlag();
+void readFirstBootFlag();
+
+
+
+
+
+
+
+
+
+
+
+
+#if DISABLED(DISABLE_M503)
+  void Config_PrintSettings(bool forReplay=false);
+#else
+  FORCE_INLINE void Config_PrintSettings(bool forReplay=false) {}
+#endif
+
+#if ENABLED(EEPROM_SETTINGS)
+  void Config_RetrieveSettings();
+#else
+  FORCE_INLINE void Config_RetrieveSettings() { Config_ResetDefault(); Config_PrintSettings(); }
+#endif
+
+#endif //CONFIGURATION_STORE_H
---
+++ Marlin_TFT_CR_V1.1.1/music.cpp
@@ -0,0 +1,79 @@
+#include "music.h"
+#include "Marlin.h"
+
+int FL[]=
+{
+//NOTE_CS6,
+//NOTE_C6,
+//NOTE_DS6,
+
+NOTE_G7,
+NOTE_FS7,
+NOTE_A7,
+
+0,
+};
+int timercount[]=
+{
+10,10,10,8
+};
+
+int printdone[]={
+NOTE_F7,
+NOTE_C7,
+NOTE_D7,
+NOTE_E7,
+NOTE_D7,
+NOTE_C7,
+NOTE_B6,
+0,  
+};
+
+int melody[] = {
+NOTE_F7,
+NOTE_C7,
+NOTE_D7,
+NOTE_E7,
+NOTE_D7,
+NOTE_G7,
+NOTE_A7,
+0,
+};
+
+int noteDurations[] = {
+
+  10,10,8,8,4,4,1,4,
+};
+
+void PowerOnMusic()
+{
+	for (int thisNote = 0; thisNote <8; thisNote++) {
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(Beeper_pin, melody[thisNote],noteDuration);
+    int pauseBetweenNotes = noteDuration * 2;
+	delay(pauseBetweenNotes);
+	noTone(8);
+	}
+}
+
+void PowerOFFMusic()
+{
+	for (int thisNote = 0; thisNote <8; thisNote++) {
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(Beeper_pin, printdone[thisNote],noteDuration);
+    int pauseBetweenNotes = noteDuration*2 ;
+	delay(pauseBetweenNotes);
+	noTone(8);
+	}
+}
+
+void FilamentLack()
+{
+for (int thisNote = 0; thisNote <4; thisNote++) {
+    int noteDuration = 1000/timercount[thisNote];
+    tone(Beeper_pin, FL[thisNote],noteDuration);
+    int pauseBetweenNotes = noteDuration*2 ;
+	delay(pauseBetweenNotes);
+	noTone(8);
+	}
+}
